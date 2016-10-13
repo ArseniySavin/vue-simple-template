ca2a922b16eb1dd44f7bb7e19bd440993e5a7575
Arhey <free.arhey@gmail.com>
2016-10-13 22:55:11 +0300

    Fix webpack.config.js
    OccurenceOrderPlugin has been renamed to OccurrenceOrderPlugin

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -50,6 +50,6 @@ if (process.env.NODE_ENV === 'production') {
         warnings: false
       }
     }),
-    new webpack.optimize.OccurenceOrderPlugin()
+    new webpack.optimize.OccurrenceOrderPlugin()
   ])
 }
